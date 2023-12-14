576f37fd5b0fef80475d8f76490c6c329eff7714
stevieWoW <34889863+stevieWoW@users.noreply.github.com>
2023-12-14 20:00:25 +0100

    Create multiplefindings.py

---
+++ multiplefindings.py
@@ -0,0 +1,30 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+faces = ['face1.jpg','face2.jpg','face3.jpg']
+img_rgb = cv.imread('whereiswaldo.jpg')
+img_gray = cv.imread('whereiswaldo.jpg',0)
+
+for f in faces:
+    face = cv.imread(f,0)
+
+    w,h = face.shape[::-1]
+
+    res = cv.matchTemplate(face,img_gray,cv.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+
+    print(min_val, max_val, min_loc, max_loc)
+
+    top_left = max_loc
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+    print(top_left, bottom_right)
+
+    cv.rectangle(img_rgb,top_left,bottom_right, (0, 255, 0), 2)
+    cv.putText(img_rgb,'Waldo', bottom_right, cv.FONT_HERSHEY_TRIPLEX,3, (0,255,0),3)
+
+plt.imshow(img_rgb)
+plt.suptitle('Ergebnis')
+plt.show()
+
+cv.imwrite('Waldogefunden.jpg', img_rgb)
